520748c125ddfabe30d63446d7618437f03a8c78
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-07 02:22:40 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5e3473948ef64d36d9a4.js"
+    "url": "webpack-runtime-c2cfc237335ef8d12492.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "147166cf009b89ca69978105abefd127"
+    "revision": "9497957b84463e312ae4c60fe345d02b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
